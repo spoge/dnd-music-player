b34557a2e7eaa73bf3f5bf7674a9dd86ba2b4cd4
spoge <sondreboge@hotmail.com>
2019-12-09 16:58:39 +0100

    Remove routing and about-page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { HashRouter as Router, Route } from "react-router-dom";
 
 import App from "./App";
-import About from "./pages/about";
 import StoreProvider from "./Store";
 
 ReactDOM.render(
   <StoreProvider>
-    <Router>
-      <div>
-        <main>
-          <Route exact path="/" component={App} />
-          <Route path="/about" component={About} />
-        </main>
-      </div>
-    </Router>
+    <App />
   </StoreProvider>,
   document.getElementById("root")
 );
