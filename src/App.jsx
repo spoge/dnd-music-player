3291c23cac649175d79bd667887da83eb653bf6c
spoge <sondreboge@hotmail.com>
2019-12-16 13:10:57 +0100

    Rename App.js => App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import "./App.scss";
 
 import PlaylistPage from "./components/PlaylistPage";
 
-// import { Store } from "./Store.js";
-
 function App() {
-  // const globalState = React.useContext(Store);
-  // const { state } = globalState;
-
-  // console.log(state);
-
   return (
     <div className="App">
       <PlaylistPage />
